74309e16cbcacadfa0be72a683345d40ae933fd0
Madelyn Papa <m23papa@siena.edu>
2024-02-15 12:01:12 -0500

    RobotFriends Updated

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({id,name,email}) => {
     return(
         <div className="bg-light-green dib br3 ma2 pa3 tc grow bw2 shadow-5">
-            <img src = {`https://robohash.org/${id}?size=200x200`} alt = "picture of robot" />
+            <img src = {`https://robohash.org/${name}?size=200x200`} alt = "picture of robot" />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -2,6 +2,10 @@ import React from "react";
 import Card from "./Card";
 
 const CardList=({robots}) => {
+  if(true){
+    throw new Error("Nooooo");
+  }
+  
   return( <div className="tc">
     {robots.map((user,i) => {
     return (
